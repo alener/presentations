b161727c3bf1f18610785d89d491f2767304a086
Alejandro Nereo Carballo <alener014@gmail.com>
2019-04-11 12:04:43 -0300

    changes in chargedata.py done in django shell

--- information/chargedata.py
+++ information/chargedata.py
@@ -1,7 +1,7 @@
 
 import json
 from .models import Creator, Presentation
-
+import datetime
 #mport presentations.settings.base
 
 with open('data.json', 'r') as f:
@@ -14,7 +14,13 @@ for i in range(len(data)):
     au = Creator(name=data[i]["creator"]["name"], profileUrl=data[i]["creator"]["profileUrl"])
     au.save()
 
-for i in range(1,len(data)+1):
-    b = Creator.objects.get(id=i)
-    pre = Presentation(id= data[i]['id'], title=data[i]['title'], thumbnail=data[i]['thumbnail'], author=b,createdAt=data[i]['createdAt'])
+
+for i in range(len(data)):
+    date_new=data[i]['createdAt']
+    date_object = datetime.datetime.strptime(date_new, '%B %d, %Y').date()
+    data[i]['createdAt']=date_object
+
+for i in range(len(data)):
+    b = Creator.objects.get(id=i+1)
+    pre = Presentation(ident= data[i]['id'], title=data[i]['title'], thumbnail=data[i]['thumbnail'], author=b,createdAt=data[i]['createdAt'])
     pre.save()
